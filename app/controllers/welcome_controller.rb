d166a72b109e65fe581ace62c0e3d4383c17aeb8
shangyukyo <shangyukyo>
2019-03-18 15:47:56 +0800

    init

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,4 +2,17 @@ class WelcomeController < ApplicationController
 
   def index
   end
+
+  def beijing_waijiao_paiqian
+
+  end
+
+  def beijing_waijiao_jianzhi_waijiao_paiqian
+  end
+
+  def beijing_waijiao_waidi_quanzhi_waijiao_paiqian
+  end
+
+  def beijing_waijiao_xuexiao_waijiao_faq
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'welcome#index'
+
+  resources :welcome do 
+    collection do
+      get :beijing_waijiao_paiqian
+      get :beijing_waijiao_jianzhi_waijiao_paiqian
+      get :beijing_waijiao_waidi_quanzhi_waijiao_paiqian
+      get :beijing_waijiao_xuexiao_waijiao_faq
+    end
+  end
 end
