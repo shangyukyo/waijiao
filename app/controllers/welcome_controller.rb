04ae2426401bb4710ec7d0a2648ea52685907c36
shangyukyo <shangyukyo>
2019-03-26 16:33:42 +0800

    init

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -15,4 +15,35 @@ class WelcomeController < ApplicationController
 
   def beijing_waijiao_xuexiao_waijiao_faq
   end
+
+  def geren_waijiao_fuwu_youshi
+  end
+
+  def geren_waijiao_fuwu_liucheng
+  end
+
+  def geren_waijiao_shoufei_qingkuang
+  end
+
+  def geren_waijiao_changjian_wenti
+  end
+  
+  def zhujia_waijiao_fuwu_youshi
+  end
+
+  def zhujia_waijiao_fuwu_liucheng
+  end
+
+  def zhujia_waijiao_jiating_xuzhi
+  end
+
+  def zhujia_waijiao_changjian_wenti
+  end
+
+  def waiguo_lietou_fuwu_liucheng
+  end
+
+  def waiguo_lietou_shoufei_qingkuang
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,18 @@ Rails.application.routes.draw do
       get :beijing_waijiao_jianzhi_waijiao_paiqian
       get :beijing_waijiao_waidi_quanzhi_waijiao_paiqian
       get :beijing_waijiao_xuexiao_waijiao_faq
+      get :geren_waijiao_fuwu_liucheng
+      get :geren_waijiao_fuwu_youshi
+      get :geren_waijiao_shoufei_qingkuang
+      get :geren_waijiao_changjian_wenti
+
+      get :zhujia_waijiao_fuwu_youshi
+      get :zhujia_waijiao_fuwu_liucheng
+      get :zhujia_waijiao_jiating_xuzhi
+      get :zhujia_waijiao_changjian_wenti
+
+      get :waiguo_lietou_fuwu_liucheng
+      get :waiguo_lietou_shoufei_qingkuang
       get :contact_us
     end
   end
