bbb50e9175b7da36fa467279ee12d3f50c87f893
shangyukyo <shangyukyo>
2020-07-29 17:24:10 +0800

    refine

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'welcome#index'
+  root to: 'welcome#index_gu_laoshi'
 
   resources :welcome do 
     collection do
