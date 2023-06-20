03944e603d2a551769607807edd62d36f78f93f1
shangyukyo <sdzc1014@163.com>
2023-06-20 09:16:31 +0800

    refine

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,5 +13,16 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 Rails.application.config.assets.precompile += %w( 
   login.css
-  admin.css
+  admin.css 
+  fusion-shortcodes.min-ver=1.7.2.css
+  animations.min-ver=1.7.2.css
+  pum-site-styles-generated.css
+  style.min-ver=5.7.2.css
+  font-awesome.min-ver=5.7.2.css
+
+  greensock-ver=1.19.0.js
+  layerslider.kreaturamedia.jquery-ver=6.7.6.js
+  layerslider.transitions-ver=6.7.6.js
+  jquery.themepunch.tools.min-ver=5.4.7.4.js
+  jquery.themepunch.revolution.min-ver=5.4.7.4.js
 )
